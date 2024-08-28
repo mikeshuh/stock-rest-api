a99ccd0d81a43f90d27c6a92f52e5fb827a066ec
Michael Huh <michael.stephen.huh@gmail.com>
2024-08-28 11:32:16 -0700

    backend init

---
+++ spring-boot-backend/src/test/java/com/mikrelin/spring_boot_backend/SpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikrelin.spring_boot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
